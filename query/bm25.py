af4e3c72deea157e4cf31cea5eef069f9511ef03
Sylphiann <hydrolilium@gmail.com>
2025-11-08 18:08:25 +0700

    [0.3.1] Associate query results to its instance's uid

--- query/bm25.py
+++ query/bm25.py
@@ -2,13 +2,6 @@ import bm25s
 from .models import Sentence
 
 def search_sentences_bm25(query: str, top_k: int = 15):
-    """
-    Retrieves all Sentence instances and performs a BM25 search.
-
-    :param query: The search query string.
-    :param top_k: The number of top results to return.
-    :return: A list of tuples: (Sentence_instance, score) for the top results.
-    """
     all_sentences = Sentence.objects.all()
     corpus = [sentence.sentence for sentence in all_sentences]
 
@@ -22,8 +15,6 @@ def search_sentences_bm25(query: str, top_k: int = 15):
     retriever.index(corpus_tokens)
 
     query_tokens = bm25s.tokenize([query])
-    
-    results_text, scores = retriever.retrieve(query_tokens, corpus=corpus, k=top_k)
     doc_ids, scores = retriever.retrieve(query_tokens, k=top_k)
 
     # Flatten
@@ -35,6 +26,6 @@ def search_sentences_bm25(query: str, top_k: int = 15):
         # Only if a result scores > 0.0
         if score > 0:
             sentence_instance = sentence_map[index]
-            ranked_results.append((sentence_instance, score))
+            ranked_results.append((int(sentence_instance.id), sentence_instance, score))
 
     return ranked_results

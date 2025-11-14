eda2b24c5158215f91d3b8a78bd2bee39cb99126
Sylphiann <hydrolilium@gmail.com>
2025-11-14 22:09:14 +0700

    [0.6.0] Restructure and dockerize program

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+echo "Running migrations..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "Creating superuser if it doesn't exist..."
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+import os
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists or missing env vars.")
+EOF
+
+echo "Starting Django server..."
+python manage.py runserver 0.0.0.0:8000
--- query/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main_view, name='main'),
-    path('query', views.search_result, name='query'),
-    path('set-sentiment', views.set_sentiment, name='set-sentiment'),
-    path('remove-sentiment', views.remove_sentiment, name='remove-sentiment'),
-]
